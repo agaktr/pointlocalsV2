4b6abbd03e58425b213f38f04e59bbd2689de66b
agaktr <katradis.agamemnon@gmail.com>
2022-10-24 19:34:00 +0300

    css

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,19 @@ Encore
     .enableSassLoader()
     .enablePostCssLoader()
 
+    .copyFiles({
+        from: './assets/img',
+
+        // optional target path, relative to the output dir
+        to: 'img/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
+
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
