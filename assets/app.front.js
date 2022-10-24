9529ae4552ed2661105f3e9c5478caafa34409b7
agaktr <katradis.agamemnon@gmail.com>
2022-10-24 19:10:24 +0300

    css

--- assets/app.front.js
+++ assets/app.front.js
@@ -7,7 +7,6 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.front.css';
-import 'Font-Awesome/css/all.css';
 
 // start the Stimulus application
 import './bootstrap';
